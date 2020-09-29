7fd3c653fe912c727c0f81a99c5c6f3400f9bee9
Liao Wen Chung <wcliao1962@gmail.com>
2020-09-29 21:16:16 +0800

    安裝&測試monolog

--- public/index.php
+++ public/index.php
@@ -5,9 +5,21 @@ use \Demo\HelloWorld as World;
 use Demo\Hello\Lara;
 use Demo\Hello;
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
 $world = new World();
 $lara= new Lara();
 $vincent= new Hello\Someone('Vincent');
 
 $mary = new \Demo\Hello\Someone('Mary');
 $john = new Demo\Hello\Someone('John');
+
+
+// create a log channel
+$log = new Logger('lara');
+$log->pushHandler(new StreamHandler('../log/my.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
